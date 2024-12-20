4c6b3f303d5995572ba5eb6ad57941a6831c7f11
Savchenko Mykhailo <mihail123456798@gmail.com>
2024-12-20 16:11:48 +0200

    1Меню

--- src/Menu.js
+++ src/Menu.js
@@ -1,37 +1,4 @@
-// import React from 'react';
 
-// class Menu extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             isOpen: false, // Состояние для отслеживания открытости меню
-//         };
-//     }
-
-//     toggleMenu = () => {
-//         this.setState((prevState) => ({
-//             isOpen: !prevState.isOpen, // Переключаем состояние
-//         }));
-//     };
-//     render(){
-//         const { isOpen } = this.state;
-//         const pages = ['Профіль', 'Контакти', 'ІНП','Поточний контроль','Резульати Аттестацій','Сессія','Розклад']; // Список страниц (можно передать через props)
-
-//         return (
-//             <div>
-//                 <button onClick={this.toggleMenu}>Меню</button> {/* Кнопка для открытия/закрытия */}
-//                 {isOpen && ( // Условный рендеринг списка
-//                     <ul>
-//                         {pages.map((page, index) => (
-//                             <li key={index}>{page}</li> // Рендерим каждую страницу
-//                         ))}
-//                     </ul>
-//                 )}
-//             </div>
-//         );
-//     }
-//     }
-// export default Menu
 import React from 'react';
 
 
